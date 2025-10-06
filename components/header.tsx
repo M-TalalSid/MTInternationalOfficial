458634c40b8f8f3c825882a05bff591836aba139
Talal Shoaib <160588772+M-TalalSid@users.noreply.github.com>
2025-10-06 08:38:49 +0500

    header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -26,11 +26,7 @@ export default function Header() {
   ]
 
   return (
-    <header
-      // className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
-      //   isScrolled ? "glass-header shadow-lg" : "bg-transparent"
-      // }`}
-    >
+    <header>
       <div className="container mx-auto px-4 py-4">
         <div className="flex items-center justify-between">
           {/* Logo */}
